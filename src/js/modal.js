c336fd0ac7964a971599f404061d250bd36e31ff
Alina <a.kotlovets@lntu.edu.ua>
2024-10-15 16:08:42 +0300

    chenge js for modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,39 +1,34 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
-//
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-//
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
-
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-    body: document.body,
-    menuBtn: document.querySelector("[data-menu-button]"),
   };
 
-  refs.openModalBtn.addEventListener("click", openModal);
-  refs.closeModalBtn.addEventListener("click", closeModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+  const modal = document.querySelector('.backdrop');
+  const body = document.body;
+
+// Відкриття модального вікна
   function openModal() {
-    refs.modal.classList.remove("is-hidden");
-    refs.body.classList.add("no-scroll");
-    refs.menuBtn.disabled = true; // Забороняємо натискання кнопки меню
+    modal.classList.remove('is-hidden');
+    body.classList.add('no-scroll'); // Додаємо клас, щоб заборонити скрол
   }
 
+// Закриття модального вікна
   function closeModal() {
-    refs.modal.classList.add("is-hidden");
-    refs.body.classList.remove("no-scroll");
-    refs.menuBtn.disabled = false; // Відновлюємо можливість натискання кнопки меню
+    modal.classList.add('is-hidden');
+    body.classList.remove('no-scroll'); // Видаляємо клас, щоб дозволити скрол
   }
+
+// Додайте обробники подій для кнопок, які відкривають і закривають модальне вікно
+  document.querySelector('[data-modal-close]').addEventListener('click', closeModal);
+  document.querySelector('[data-modal-open]').addEventListener('click', openModal);
+
 })();
